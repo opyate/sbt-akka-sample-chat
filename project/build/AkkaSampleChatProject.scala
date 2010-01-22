65b79c2981471a95415260c683c022cfe56111cb
molecule <molecule@molecules-macbook-pro.local>
2010-01-21 23:19:31 -0500

    initial commit

---
+++ project/build/AkkaSampleChatProject.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+class AkkaSampleChatProject(info: ProjectInfo) extends DefaultProject(info){
+    val guiceyfruit = "GuiceyFruit" at "http://guiceyfruit.googlecode.com/svn/repo/releases/"
+    val databinder = "DataBinder" at "http://databinder.net/repo"
+    val configgy = "Configgy" at "http://www.lag.net/repo"
+    val multiverse = "Multiverse" at "http://multiverse.googlecode.com/svn/maven-repository/snapshots"
+    val jBoss = "jBoss" at "http://repository.jboss.org/maven2"
+    val akka_repo = "Akka Maven Repository" at "http://scalablesolutions.se/akka/repository"
+
+    val akka = "se.scalablesolutions.akka" % "akka-core" % "0.6"
+    val akka_redis = "se.scalablesolutions.akka" % "akka-persistence-redis" % "0.6"
+    val akka_util = "se.scalablesolutions.akka" % "akka-util" % "0.6"
+}
